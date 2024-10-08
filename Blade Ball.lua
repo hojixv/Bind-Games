05b035a075f7ad21c24ac6c38f2d3fdd04c52904
hojixv <153679337+hojixv@users.noreply.github.com>
2024-10-08 15:48:11 -0400

    Blade Ball.lua

---
+++ Blade Ball.lua
@@ -0,0 +1,40 @@
+script_key='';
+Bladeball = {
+    ['AutoParry'] = true;
+    ['Debug'] = true; --// Leave on
+    ['SetCore'] = {}; 
+    ['Threading'] = {
+        ['Min'] = 10;
+        ['Max'] = 10;
+    };
+    ['BypassGame'] = {
+        ['Memory'] = true;
+    };
+    ['Distance'] = {
+        ['Max'] = 10;
+        ['Min'] = 10;
+    };
+    ['Delay'] = 0.01;
+    ['Mode'] = 'Auto'; --// Fast, Slow, Optimal, Perfect
+    ['Advance'] = {
+        ['Visuals'] = {
+            ['Ball'] = false;
+            ['XYZ'] = false;
+            ['Xyz'] = {
+                ['Z'] = 10;
+                ['Y'] = 50;
+                ['X'] = 5;
+                ['Visualize'] = {
+                    ['Circle'] = false;
+                    ['Player'] = false;
+                    ['HitSounds'] = false;
+                    ['Hit_Detection'] = true;
+                    ['Hitlogs'] = false;
+                };
+            };
+            ['CustomMode'] = ''; --// K, Op, Perfect
+        };
+    };
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bc967131f3edd6c4dc26cdea41048275.lua"))()
