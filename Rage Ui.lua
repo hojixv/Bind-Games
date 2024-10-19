2a48583f2f94acd617e580f7e3bc872b2ab927d6
hojixv <153679337+hojixv@users.noreply.github.com>
2024-10-19 17:56:16 -0400

    Ui.lua

---
+++ Rage Ui.lua
@@ -0,0 +1,7 @@
+getgenv().Debug = true --// If ur first Loading it Please Leave on True
+getgenv().Insets = true --// If ur first Loading it Please Leave on True
+getgenv().Solara = false
+getgenv().Stealth = false 
+
+script_key='';
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e1db4e340267095ae4ca565ea7f481e3.lua"))()
